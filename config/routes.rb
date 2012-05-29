23d3371ef39a8b8667dbc73246c69f7d046e11b3
Dao Van Mai <daomai3c@gmail.com>
2012-05-29 22:55:57 +0700

    message

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ StaticPages::Application.routes.draw do
   get "static_pages/contact"
 
   get "static_pages/about"
+  
+  root to: "static_pages#home"
+  match "/help", to: "static_pages#help"
+  match "/contact", to: "static_pages#contact"
+  match "/about", to: "static_pages#about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +61,6 @@ StaticPages::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,46 +4,42 @@ describe "Static Pages" do
   describe "Home page" do
     
     it "should have the content 'home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('home')
     end
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                         :text => "Sample | Home")
-    end
-    it "should have the content 'home'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1',:text => 'home')
     end
   end
   
   describe "Help page" do
     it "should have the content 'help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "Sample | Help")
     end
     it "should have the content 'help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1',:text => 'help')
     end
   end
   
   describe "About page" do
     it "should have the content 'about'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('about')
     end
   end
   
   describe "Contact page" do
     it "should have the content 'contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_content('contact')
     end
   end
