9879bf9f70c429983b0f288f02c07bc1813fbe46
Dao Van Mai <daomai3c@gmail.com>
2012-05-30 08:37:10 +0700

    mesage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 StaticPages::Application.routes.draw do
+  get "users/new"
+
   get "static_pages/home"
 
   get "static_pages/help"
@@ -11,7 +13,7 @@ StaticPages::Application.routes.draw do
   match "/help", to: "static_pages#help"
   match "/contact", to: "static_pages#contact"
   match "/about", to: "static_pages#about"
-
+  match "/signup", to: "users#new"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,48 @@
 require 'spec_helper'
 
+
+
 describe "Static Pages" do
+  
+  subject { page }
+  
   describe "Home page" do
-    
-    it "should have the content 'home'" do
+    before { visit root_path } 
+
+    it { should have_selector('h1', :text => 'Sample App') }
+    it { should have_selector 'title',:text => "Sample | Home" }
+    it "should have the right links on the layout" do
       visit root_path
-      page.should have_content('home')
-    end
-    it "should have the title 'Home'" do
-      visit root_path
-      page.should have_selector('title',
-                        :text => "Sample | Home")
+      click_link "About"
+      page.should have_selector 'title', text: "Sample | About"
+      click_link "Help"
+      page.should have_selector 'title', text: "Sample | Help"
+      click_link "Contact"
+      page.should have_selector 'title',text: "Sample | Contact"
+      click_link "Home"
+      page.should have_selector 'title', text: "Sample | Home"
+      click_link "Sign up now!"
+      page.should have_selector 'title', text: "Sample | Sign up"
+      click_link "sample app"
+      page.should have_selector 'title', text: "Sample | Home"
     end
   end
   
   describe "Help page" do
-    it "should have the content 'help'" do
-      visit help_path
-      page.should have_content('help')
-    end
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-                        :text => "Sample | Help")
-    end
-    it "should have the content 'help'" do
-      visit help_path
-      page.should have_selector('h1',:text => 'help')
-    end
+    before {visit help_path}
+    it {should have_content('help')}
+    it {should have_selector('title',:text => "Sample | Help")}
   end
   
   describe "About page" do
-    it "should have the content 'about'" do
-      visit about_path
-      page.should have_content('about')
-    end
+    before {visit about_path}
+    it { should have_content('about')}
+    it {should have_selector('title',:text => 'Sample | About')}
   end
   
   describe "Contact page" do
-    it "should have the content 'contact'" do
-      visit contact_path
-      page.should have_content('contact')
-    end
+    before {visit contact_path}
+    it {should have_selector('body', :text => 'contact')}
+    it {should have_selector('title', :text => 'Sample | Contact')}
   end
 end
